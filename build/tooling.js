2e81ee09749bed9e395b9b5235955e9f6f8a1abd
Greg Miller <hello@gregmiller.io>
2023-06-02 15:57:14 -0400

    Render build updates

--- build/tooling.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";document.addEventListener("DOMContentLoaded",(()=>{window.acf&&(console.log("Hello from "),window.acf.addAction("render_block_preview/type=acf/tooling",initMyBlock))}))})();
